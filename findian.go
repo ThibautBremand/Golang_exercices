a3653f66a76696ebc74a213016e3beff169641a6
Thibaut BREMAND <thibaut.bremand@gmail.com>
2019-09-19 23:48:10 +0200

    better way to find first and last chars of a string

--- findian.go
+++ findian.go
@@ -1,6 +1,6 @@
 /*
-Write a program which prompts the user to enter a string. The program searches through the entered string for the characters ‘i’, ‘a’, and ‘n’. 
-The program should print “Found!” if the entered string starts with the character ‘i’, ends with the character ‘n’, and contains the character ‘a’. 
+Write a program which prompts the user to enter a string. The program searches through the entered string for the characters ‘i’, ‘a’, and ‘n’.
+The program should print “Found!” if the entered string starts with the character ‘i’, ends with the character ‘n’, and contains the character ‘a’.
 The program should print “Not Found!” otherwise. The program should not be case-sensitive, so it does not matter if the characters are upper-case or lower-case.
 
 Examples: The program should print “Found!” for the following example entered strings, “ian”, “Ian”, “iuiygaygn”, “I d skd a efju N”. The program should print “Not Found!” for the following strings, “ihhhhhn”, “ina”, “xian”.
@@ -11,10 +11,10 @@ Submit your source code for the program, “findian.go”.
 package main
 
 import (
-	"fmt"
-	"strings"
 	"bufio"
+	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -25,17 +25,18 @@ func main() {
 	// Use of scanner instead of fmt.Scanln because the fmt package intentionally filters out whitespaces which is not acceptable in our case.
 	scanner := bufio.NewScanner(os.Stdin)
 	if scanner.Scan() {
-	    input = scanner.Text()
+		input = scanner.Text()
 
-	    // Not case-sensitive
-	    input = strings.ToLower(input)
-	    fmt.Println("The value you have entered:", input)
+		// Not case-sensitive
+		input = strings.ToLower(input)
+		fmt.Println("The value you have entered:", input)
 
-	    // Check if the first character is 'i', the last one is 'n' and the string contains an 'a'
-	    if (string(input[0]) == "i" && string(input[len(input) - 1]) == "n" && strings.Contains(input, "a")) {
-	    	fmt.Println("Found!")
-	    } else {
-	    	fmt.Println("Not Found!")
-	    }
+		// Check if the first character is 'i', the last one is 'n' and the string contains an 'a'
+		//if (string(input[0]) == "i" && string(input[len(input) - 1]) == "n" && strings.Contains(input, "a")) {
+		if strings.HasPrefix(input, "i") && strings.HasSuffix(input, "n") && strings.Contains(input, "a") {
+			fmt.Println("Found!")
+		} else {
+			fmt.Println("Not Found!")
+		}
 	}
 }
